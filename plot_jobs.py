d7a38ef321199a5b8a3c8342a7ccebc1a3072dd2
csecht <spam-me@earthlink.net>
2022-07-26 17:37:13 -0500

    Ver 0.1.8: move args and quit functions modules

--- plot_jobs.py
+++ plot_jobs.py
@@ -37,15 +37,14 @@ Developed in Python 3.8-3.9.
 # Copyright (C) 2022 C.S. Echt, under GNU General Public License
 
 # Standard library imports
-import argparse
 import sys
 from pathlib import Path
 
 # Local application imports
 import plot_utils
-from plot_utils import (path_check, platform_check as chk,
-                        markers as mark, project_groups as grp,
-                        reports)
+from plot_utils import (path_check, reports, utils,
+                        markers as mark,
+                        project_groups as grp)
 
 # Third party imports (tk may not be included with some Python installations).
 try:
@@ -80,65 +79,6 @@ except (ImportError, ModuleNotFoundError) as import_err:
           '   See also: https://tkdocs.com/tutorial/install.html \n\n'
           f'Error message:\n{import_err}')
     sys.exit(1)
-
-
-def manage_args() -> bool:
-    """Allow handling of command line arguments.
-
-    :return: True if --test argument used (default: False).
-    """
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--about',
-                        help='Provide description, version, GNU license',
-                        action='store_true',
-                        default=False)
-    parser.add_argument('--test',
-                        help='Plot _test data instead of your job_log data.',
-                        action='store_true',
-                        default=False,
-                        )
-
-    args = parser.parse_args()
-
-    if args.about:
-        print(__doc__)
-        print(f'{"Author:".ljust(13)}', plot_utils.__author__)
-        print(f'{"Version:".ljust(13)}', plot_utils.__version__)
-        print(f'{"Status:".ljust(13)}', plot_utils.__dev_status__)
-        print(f'{"URL:".ljust(13)}', plot_utils.URL)
-        print(plot_utils.__copyright__)
-        print(plot_utils.LICENSE)
-        sys.exit(0)
-
-    return args.test
-
-
-def quit_gui(keybind=None) -> None:
-    """
-    Error-free and informative exit from the program.
-    Called from widget or keybindings.
-    Explicitly closes all Matplotlib objects and their parent tk window
-    when the user closes the plot window with the system's built-in
-    close window icon ("X") or key command. This is required to cleanly
-    exit and close the tk thread running Matplotlib.
-
-    :param keybind: Implicit event passed from bind().
-    """
-
-    print('\n*** User quit the program. ***\n')
-
-    # pylint: disable=broad-except
-    try:
-        plt.close('all')
-        canvas_window.update_idletasks()
-        canvas_window.after(200)
-        canvas_window.destroy()
-    except Exception as err:
-        print(f'An error occurred: {err}')
-        sys.exit('Program exit with unexpected condition.')
-
-    return keybind
 
 
 class TaskDataFrame:
@@ -327,16 +267,6 @@ class PlotTasks(TaskDataFrame):
     # https://stackoverflow.com/questions/472000/usage-of-slots
     # https://towardsdatascience.com/understand-slots-in-python-e3081ef5196d
     __slots__ = (
-        # plot_utils local module attributes
-        '__dev_status__', '__copyright__', 'URL', 'LICENSE',
-        'MARKER_STYLE', 'CBLIND_COLOR', 'DARK_GRAY', 'LIGHT_GRAY',
-        'markers', 'markers_cycle',
-        'MY_OS', 'CFGFILE', 'TESTFILE',
-        'PROJECTS', 'PROJ_TO_REPORT', 'ALL_EXCLUDED', 'GW_SERIES_EXCLUDED',
-        'ALL_INCLUSIVE', 'GW_SERIES_INCLUSIVE', 'CHKBOX_LABELS', 'GW_SERIES',
-        # __main__ attributes
-        'do_test', 'datapath', 'img', 'canvas_window',
-        # Instance attributes
         '_test',
         'marker_size', 'marker_scale', 'dcnt_size', 'pick_radius',
         'fig', 'ax1', 'ax2',
@@ -420,10 +350,10 @@ class PlotTasks(TaskDataFrame):
         canvas_window.rowconfigure(0, weight=1)
         canvas_window.columnconfigure(0, weight=1)
         canvas_window.configure(bg=mark.CBLIND_COLOR['blue'])
-        canvas_window.protocol('WM_DELETE_WINDOW', quit_gui)
+        canvas_window.protocol('WM_DELETE_WINDOW', utils.quit_gui)
 
-        canvas_window.bind_all('<Escape>', quit_gui)
-        canvas_window.bind('<Control-q>', quit_gui)
+        canvas_window.bind_all('<Escape>', utils.quit_gui)
+        canvas_window.bind('<Control-q>', utils.quit_gui)
 
         canvas = backend.FigureCanvasTkAgg(self.fig, master=canvas_window)
 
@@ -448,7 +378,7 @@ class PlotTasks(TaskDataFrame):
                                     )
         # Because macOS tool icon images won't render properly,
         #   need to provide text description for tool functions.
-        if chk.MY_OS == 'dar':
+        if sys.platform == 'darwin':
             tool_lbl = tk.Label(canvas_window,
                                 text='Home Fwd Back | Pan  Zoom | Save',
                                 font=('TkTooltipFont', 8))
@@ -1212,7 +1142,7 @@ if __name__ == "__main__":
     # System platform and version checks are run in plot_utils __init__.py
     #   Program exits if checks fail.
 
-    do_test = manage_args()  # Function returns boolean.
+    do_test = utils.manage_args()  # Function returns boolean.
 
     if not do_test:
         datapath = path_check.set_datapath()
---
+++ plot_utils/utils.py
@@ -0,0 +1,66 @@
+import __main__
+import argparse
+import sys
+
+import matplotlib.pyplot as plt
+
+import plot_utils
+
+
+def manage_args() -> bool:
+    """Allow handling of command line arguments.
+
+    :return: True if --test argument used (default: False).
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--about',
+                        help='Provide description, version, GNU license',
+                        action='store_true',
+                        default=False)
+    parser.add_argument('--test',
+                        help='Plot _test data instead of your job_log data.',
+                        action='store_true',
+                        default=False,
+                        )
+
+    args = parser.parse_args()
+
+    if args.about:
+        print(__doc__)
+        print(f'{"Author:".ljust(13)}', plot_utils.__author__)
+        print(f'{"Version:".ljust(13)}', plot_utils.__version__)
+        print(f'{"Status:".ljust(13)}', plot_utils.__dev_status__)
+        print(f'{"URL:".ljust(13)}', plot_utils.URL)
+        print(plot_utils.__copyright__)
+        print(plot_utils.LICENSE)
+        sys.exit(0)
+
+    return args.test
+
+
+def quit_gui(keybind=None) -> None:
+    """
+    Error-free and informative exit from the program.
+    Called from widget or keybindings.
+    Explicitly closes all Matplotlib objects and their parent tk window
+    when the user closes the plot window with the system's built-in
+    close window icon ("X") or key command. This is required to cleanly
+    exit and close the tk thread running Matplotlib.
+
+    :param keybind: Implicit event passed from bind().
+    """
+
+    print('\n*** User quit the program. ***\n')
+
+    # pylint: disable=broad-except
+    try:
+        plt.close('all')
+        __main__.canvas_window.update_idletasks()
+        __main__.canvas_window.after(200)
+        __main__.canvas_window.destroy()
+    except Exception as err:
+        print(f'An error occurred: {err}')
+        sys.exit('Program exit with unexpected condition.')
+
+    return keybind
