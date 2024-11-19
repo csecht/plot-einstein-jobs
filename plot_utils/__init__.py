99ee46a48293a0f70f7d567ef76da951a0908daa
craig <spam-me@earthlink.net>
2024-11-18 18:30:39 -0600

    ver. 0.2.22: Updated ver# and copyright

--- plot_utils/__init__.py
+++ plot_utils/__init__.py
@@ -4,9 +4,9 @@ These constants are used with the --about command line argument or button.
 
 # Development status standards: https://pypi.org/classifiers/
 __author__ = 'Craig S. Echt'
-__version__: str = '0.2.21'
+__version__: str = '0.2.22'
 __status__ = 'Development Status :: 4 - Beta'
-__copyright__ = 'Copyright (C) 2022 C.S. Echt, under GNU General Public License'
+__copyright__ = 'Copyright (C) 2022-2024 C.S. Echt, under GNU General Public License'
 __license__ = """
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
